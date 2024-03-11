8979b45589ebe05ce10bb595597f73e3fb790128
Fouzia Sultana <fouziasultana193@gmail.com>
2024-03-11 23:04:52 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'black':'rgb(6, 0, 71)',
+        'pink':'rgb(179, 0, 94)',
+        'marron': '#640d23',
+        'off-white': '#f3e9ea',
+        'navy': '#1e3650',
+        'pale-blue':'#b5d8ff',
+        'teal': '#24728a',
+        'vermillion': '#c03711',
+        'white': '#fff',
+        'peach': '#fed8ce'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
+
